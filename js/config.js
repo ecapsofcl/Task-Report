f66b6d026d887939264da25155c612112921d5e8
ecapsofcl <ecapscomputers123@gmail.com>
2026-08-24 10:10:25 +0530

    Update API_URL to new deployment URL

--- js/config.js
+++ js/config.js
@@ -1,2 +1,2 @@
 // Paste your Apps Script Web App deployment URL here (ends in /exec)
-const API_URL = 'https://script.google.com/macros/s/AKfycbwG0nq0z7zpulGzFNJHVWG37x5Vd-vgj2NKyLpILsY4HwAOWq3Wz2Gf1_LDbvmXAjwm/exec';
+const API_URL = 'https://script.google.com/macros/s/AKfycbyPGBhat3Uni5cyMBMmxxh5bvbFwdyrBpzJcS3AVsCTFAcUHwSDzK_Fi4L544b8Wils/exec';
